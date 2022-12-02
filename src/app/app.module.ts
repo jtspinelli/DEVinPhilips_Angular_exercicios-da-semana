657e17d455dc43afd696ffdfa78c4f0145f99b4a
jtspinelli <contato@jonathanspinelli.com>
2022-12-02 19:22:33 -0300

    add part1/FormCadastroMateriasComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormCadastroMateriasComponent } from './part1/form-cadastro-materias/form-cadastro-materias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormCadastroMateriasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/part1/form-cadastro-materias/form-cadastro-materias.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-cadastro-materias',
+  templateUrl: './form-cadastro-materias.component.html',
+  styleUrls: ['./form-cadastro-materias.component.scss']
+})
+export class FormCadastroMateriasComponent {
+
+}
