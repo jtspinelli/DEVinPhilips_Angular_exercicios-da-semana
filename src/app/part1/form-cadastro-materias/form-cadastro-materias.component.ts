f52eb4ca402397b8461e4b9d9fce1cacc952745f
jtspinelli <contato@jonathanspinelli.com>
2022-12-02 19:27:46 -0300

    rename FormCadastroMateriasComponent selector

--- src/app/part1/form-cadastro-materias/form-cadastro-materias.component.ts
+++ src/app/part1/form-cadastro-materias/form-cadastro-materias.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-form-cadastro-materias',
+  selector: 'form-cadastro-materias',
   templateUrl: './form-cadastro-materias.component.html',
   styleUrls: ['./form-cadastro-materias.component.scss']
 })
