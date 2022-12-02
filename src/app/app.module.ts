534b5fe883a75fa25cc1c555cd88b7e53402b180
jtspinelli <contato@jonathanspinelli.com>
2022-12-02 19:23:11 -0300

    add part1/NavbarComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormCadastroMateriasComponent } from './part1/form-cadastro-materias/form-cadastro-materias.component';
+import { NavbarComponent } from './part1/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormCadastroMateriasComponent
+    FormCadastroMateriasComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
