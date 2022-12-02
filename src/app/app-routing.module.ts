e6b8d2d4e3e001d153d8c801fa1ec05750823396
jtspinelli <contato@jonathanspinelli.com>
2022-12-02 19:34:51 -0300

    add rout to Part1AppComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Part1AppComponent} from "./part1/part1.app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'part1', component: Part1AppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
