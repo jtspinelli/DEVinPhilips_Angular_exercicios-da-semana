b27fc925d68be2e0308c71c9778e1f66556be1ee
jtspinelli <contato@jonathanspinelli.com>
2022-12-03 12:38:35 -0300

    add route to M1S08 NotifyAppComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Part1AppComponent } from "./M1S09/part1/part1.app.component";
 import { Part2AppComponent } from "./M1S09/part2/part2.app.component";
 import { Part3AppComponent } from "./M1S09/part3/part3.app.component";
 import { Part4AppComponent } from "./M1S09/part4/part4.app.component";
+import { NotifyAppComponent } from "./M1S08/notify.app.component";
 
 const routes: Routes = [
   {path: 'm1s09/part1', component: Part1AppComponent},
   {path: 'm1s09/part2', component: Part2AppComponent},
   {path: 'm1s09/part3', component: Part3AppComponent},
-  {path: 'm1s09/part4', component: Part4AppComponent}
+  {path: 'm1s09/part4', component: Part4AppComponent},
+  {path: 'm1s08', component: NotifyAppComponent}
 ];
 
 @NgModule({
