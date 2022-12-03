17df2a3b51148698c44a98c871dd28728d4ee03e
jtspinelli <contato@jonathanspinelli.com>
2022-12-03 09:05:11 -0300

    refactorings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormCadastroMateriasComponent } from './part1/form-cadastro-materias/form-cadastro-materias.component';
-import { NavbarComponent } from './part1/navbar/navbar.component';
-import {Part1AppComponent} from "./part1/part1.app.component";
-import {Part2AppComponent} from "./part2/part2.app.component";
-import { FavoriteSeriesCardComponent } from './part2/favorite-series-card/favorite-series-card.component';
+import { FormCadastroMateriasComponent } from './M1S09/part1/form-cadastro-materias/form-cadastro-materias.component';
+import { NavbarComponent } from './M1S09/part1/navbar/navbar.component';
+import {Part1AppComponent} from "./M1S09/part1/part1.app.component";
+import {Part2AppComponent} from "./M1S09/part2/part2.app.component";
+import { FavoriteSeriesCardComponent } from './M1S09/part2/favorite-series-card/favorite-series-card.component';
 
 @NgModule({
   declarations: [
