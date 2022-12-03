2b262517ae3a270b3d0f0ec4a5f18db9f94122b3
jtspinelli <contato@jonathanspinelli.com>
2022-12-03 09:38:59 -0300

    add M1S09/part4 Part4AppComponent body stylization on init

--- src/app/M1S09/part4/part4.app.component.ts
+++ src/app/M1S09/part4/part4.app.component.ts
@@ -1,10 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'part4-root',
   templateUrl: './part4.app.component.html',
   styleUrls: ['./part4.app.component.scss']
 })
-export class Part4AppComponent {
-
+export class Part4AppComponent implements OnInit{
+  ngOnInit() {
+    document.body.classList.add("m1s09-part4-body");
+  }
 }
