377c1c43014cc5883eccbca5850045367467b328
jtspinelli <contato@jonathanspinelli.com>
2022-12-03 08:43:27 -0300

    rename Part2 FavoriteSeriesCardComponent selector

--- src/app/part2/favorite-series-card/favorite-series-card.component.ts
+++ src/app/part2/favorite-series-card/favorite-series-card.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-favorite-series-card',
+  selector: 'favorite-series-card',
   templateUrl: './favorite-series-card.component.html',
   styleUrls: ['./favorite-series-card.component.scss']
 })
