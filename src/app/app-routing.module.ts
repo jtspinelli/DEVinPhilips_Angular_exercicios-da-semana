ec4bf23ec7a431a112170c97de0a15120f6e03c5
jtspinelli <contato@jonathanspinelli.com>
2022-12-03 09:09:08 -0300

    add route to M1S09/part3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {Part1AppComponent} from "./M1S09/part1/part1.app.component";
-import {Part2AppComponent} from "./M1S09/part2/part2.app.component";
+import { Part1AppComponent } from "./M1S09/part1/part1.app.component";
+import { Part2AppComponent } from "./M1S09/part2/part2.app.component";
+import { Part3AppComponent } from "./M1S09/part3/part3.app.component";
 
 const routes: Routes = [
   {path: 'm1s09/part1', component: Part1AppComponent},
-  {path: 'm1s09/part2', component: Part2AppComponent}
+  {path: 'm1s09/part2', component: Part2AppComponent},
+  {path: 'm1s09/part3', component: Part3AppComponent}
 ];
 
 @NgModule({
