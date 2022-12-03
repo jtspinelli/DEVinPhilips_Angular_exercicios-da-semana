5e62ce3bc5436335a05850545dfe9ed8a796cc07
jtspinelli <contato@jonathanspinelli.com>
2022-12-03 08:46:23 -0300

    add Part2AppComponent character property

--- src/app/part2/part2.app.component.ts
+++ src/app/part2/part2.app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./part2.app.component.scss']
 })
 export class Part2AppComponent {
-  title = 'exercicios-da-semana';
+  character:any = {
+    nome: 'Kleo Straub',
+    imagem: 'assets/Kleo.jpg',
+    descricao: 'Uma ex-espiã da Alemanha Oriental, Kleo possui sede de vingança contra todos aqueles que a traíram.',
+    ator: {
+      prefixo: 'atriz',
+      nome: 'Jella Haase',
+      link: 'https://en.wikipedia.org/wiki/Jella_Haase'
+    },
+    skills: [
+      {nome: 'vingança', stars: 5},
+      {nome: 'destreza', stars: 5},
+      {nome: 'social', stars: 1}
+    ]
+  }
 }
