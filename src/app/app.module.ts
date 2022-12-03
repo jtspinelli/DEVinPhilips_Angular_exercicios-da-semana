37eef7c88174c6521e6d96e21cf2386d91b1e6d7
jtspinelli <contato@jonathanspinelli.com>
2022-12-03 09:33:48 -0300

    add BrowserModule import to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +29,7 @@ import { BuscaComponent } from './M1S09/part4/busca/busca.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
