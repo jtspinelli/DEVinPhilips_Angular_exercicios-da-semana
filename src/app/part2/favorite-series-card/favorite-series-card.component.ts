1b8d10d9afb38cd682a5a521b6bd21ba1a22bed6
jtspinelli <contato@jonathanspinelli.com>
2022-12-03 08:42:29 -0300

    add Part2 FavoriteSeriesCardComponent

---
+++ src/app/part2/favorite-series-card/favorite-series-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-series-card',
+  templateUrl: './favorite-series-card.component.html',
+  styleUrls: ['./favorite-series-card.component.scss']
+})
+export class FavoriteSeriesCardComponent {
+
+}
