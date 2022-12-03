d4785d27601b48d326bad6b1d286ead1b279a148
jtspinelli <contato@jonathanspinelli.com>
2022-12-03 09:28:37 -0300

    add M1S09/part4 Part4AppComponent ts file configs

---
+++ src/app/M1S09/part4/part4.app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'part4-root',
+  templateUrl: './part4.app.component.html',
+  styleUrls: ['./part4.app.component.scss']
+})
+export class Part4AppComponent {
+
+}
