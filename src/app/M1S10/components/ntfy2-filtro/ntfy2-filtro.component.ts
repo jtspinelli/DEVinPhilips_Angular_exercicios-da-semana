463b09c7ba522f81b35d06c0111eb69fff78bfd4
jtspinelli <contato@jonathanspinelli.com>
2022-12-11 00:07:12 -0300

    M1S10 filter: handle style on active

--- src/app/M1S10/components/ntfy2-filtro/ntfy2-filtro.component.ts
+++ src/app/M1S10/components/ntfy2-filtro/ntfy2-filtro.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import {Component, Output, ViewChildren, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'ntfy2-filtro',
@@ -7,8 +7,26 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class Ntfy2FiltroComponent {
   @Output() filter: EventEmitter<string> = new EventEmitter<string>();
+  @ViewChildren('li') filterButtons:any;
 
-  emitFilter(type: string) {
+  filterOptions = [
+    {class: 'list-group-item btn-outline-secondary active', type: 'todos', textContent: 'Todos' },
+    {class: 'list-group-item btn-outline-secondary', type: 'nao-lidos', textContent: 'Não lidos' },
+    {class: 'list-group-item btn-outline-secondary', type: 'lidos', textContent: 'Lidos' }
+  ];
+
+  emitFilter(event:any, type: string) {
+    this.updateActiveFilterButton(event);
     this.filter.emit(type);
+  }
+
+  updateActiveFilterButton(event:any) {
+    this.filterButtons._results.forEach(filterButton => {
+      filterButton.nativeElement.classList.remove('active');
+
+      if(filterButton.nativeElement.textContent === event.target.textContent) {
+        filterButton.nativeElement.classList.add('active');
+      }
+    });
   }
 }
